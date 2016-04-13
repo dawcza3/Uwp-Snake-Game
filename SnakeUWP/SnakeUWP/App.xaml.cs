dbad1c887fe427be1c00384a6664c5179f6daffe
Dawid Czaja <dawcza3@outlook.com>
2016-04-13 11:54:45 +0200

    Funkcjonalność do testów

--- SnakeUWP/SnakeUWP/App.xaml.cs
+++ SnakeUWP/SnakeUWP/App.xaml.cs
@@ -23,7 +23,7 @@ namespace SnakeUWP
     sealed partial class App : Application
     {
         /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
+        /// Initializes the singl asdaeton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
         public App()
