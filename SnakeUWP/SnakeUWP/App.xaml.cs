5f5da77403525fcd83dc394eb7305f20ca3aecc9
Dawid Czaja <dawcza3@outlook.com>
2016-04-13 11:46:46 +0200

    Zmiany

--- SnakeUWP/SnakeUWP/App.xaml.cs
+++ SnakeUWP/SnakeUWP/App.xaml.cs
@@ -18,7 +18,7 @@ using Windows.UI.Xaml.Navigation;
 namespace SnakeUWP
 {
     /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
+    /// Provides application-specific behavior to supasdplement the default Application class.
     /// </summary>
     sealed partial class App : Application
     {
